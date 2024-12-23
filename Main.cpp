395692e58bb53d3e6ef5bfc20056d0d126fdfd63
Artyom <artiom.vaulin2014@yandex.ru>
2024-12-23 19:48:30 +0500

    Улучшил код

--- Main.cpp
+++ Main.cpp
@@ -4,46 +4,56 @@
 
 using namespace std;
 
-double fExact(double x)
+// Вычисление точного значения функции
+double exactFunction(double x)
 {
     return (0.25 * log((1 + x) / (1 - x))) + (0.5 * atan(x));
 }
 
-double powerSeriesN(double x, int n)
+// Вычисление ряда с заданным числом членов (рекурсивно)
+double calculateSeriesByN(double x, int n)
 {
     if (n == 0)
         return x; // Первый член ряда
 
-    double prevTerm = powerSeriesN(x, n - 1);
-    return prevTerm + pow(x, 4 * n + 1) / (4 * n + 1);
+    double prevTerm = calculateSeriesByN(x, n - 1);
+    double currentTerm = pow(x, 4 * n + 1) / (4 * n + 1);
+    return prevTerm + currentTerm;
 }
 
-double powerSeriesE(double x, double e, double term = 0, int n = 0)
+// Вычисление ряда с заданной точностью (рекурсивно)
+double calculateSeriesByE(double x, double tolerance, double term = 0, int n = 0)
 {
     double currentTerm = pow(x, 4 * n + 1) / (4 * n + 1);
-    if (fabs(currentTerm) <= e)
+    if (fabs(currentTerm) <= tolerance)
         return term + currentTerm;
 
-    return powerSeriesE(x, e, term + currentTerm, n + 1);
+    return calculateSeriesByE(x, tolerance, term + currentTerm, n + 1);
 }
 
 int main()
 {
-    double a = 0.1;
-    double b = 0.8;
-    int k = 10;
-    int n = 3;
-    double e = 0.0001;
-    double step = (b - a) / k;
+    const double start = 0.1;
+    const double end = 0.8;
+    const int steps = 10;
+    const int maxTerms = 3;
+    const double tolerance = 0.0001;
+    const double stepSize = (end - start) / steps;
 
     cout << fixed << setprecision(6);
-    for (double x = a; x <= b; x += step)
-    {
-        double Sn = powerSeriesN(x, n);
-        double Se = powerSeriesE(x, e);
-        double y = fExact(x);
+    cout << "  x        Sn         Se         y" << endl;
+    cout << "-------------------------------------" << endl;
 
-        cout << "x= " << x << " Sn = " << Sn << " Se = " << Se << " y = " << y << endl;
+    for (double x = start; x <= end; x += stepSize)
+    {
+        double seriesByN = calculateSeriesByN(x, maxTerms);
+        double seriesByE = calculateSeriesByE(x, tolerance);
+        double exactValue = exactFunction(x);
+
+        cout << setw(7) << x
+            << setw(10) << seriesByN
+            << setw(10) << seriesByE
+            << setw(10) << exactValue << endl;
     }
 
     return 0;
